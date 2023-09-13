e0ead75cfaf73ae7d69075aab301ce732ca4bd62
B3rn4z86 <alessandro.bernardini1986@gmail.com>
2023-09-13 08:09:59 +0200

    kk

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,20 @@
-const mailList = ['prova1@mail.it', 'prova2@mail.it', 'prova3@mail.it', 'prova4@mail.it', 'prova5@mail.it'];
+const mailList = ['prova1@mail.it', 'prova2@mail.it', 'prova3@mail.it', 
+'prova4@mail.it', 'prova5@mail.it'];
 
 const btnAccesso = document.getElementById("accedi");
 
 const mailInserita = document.getElementById("email");
 
-let mex = document.getElementById("msg");
+
 
 btnAccesso.addEventListener("click", function () {
   const emailUtente = mailInserita.value;
   if (mailList.includes(emailUtente)) {
-    mex.innerHTML("Benvenuto");
+    document.getElementById("msg").innerHTML = "Benvenuto!";
+    document.getElementById("hide").className = "tophide"
   } else {
-    alert("Non sei registrato");
+    document.getElementById("msg").innerHTML = "Registrati per accedere";
+    ;
   }
 });
 
